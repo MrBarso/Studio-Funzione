506e132b046e3c07fe22a58fdc3adaa8f41a786e
MrBarso <andbars2911@gmail.com>
2024-03-27 19:42:41 +0100

    Start

---
+++ Function_Input.py
@@ -0,0 +1,4 @@
+#in this file I will get the input function and I will translate and simplify it
+
+def input():
+    print("prova")
---
+++ main.py
@@ -0,0 +1,4 @@
+#in this file I combine every other file in order to do a complete study and graph of the function
+
+#importing the methods I need
+import Function_Input
