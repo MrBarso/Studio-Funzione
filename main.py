0f0be1a1dedc9e4e2c8ae2a3a331976e61909cc1
MrBarso <andbars2911@gmail.com>
2024-03-27 20:02:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 #in this file I combine every other file in order to do a complete study and graph of the function
 
+#setting up graphic interface
+import customtkinter
+
+customtkinter.set_appearance_mode()
+
+root = customtkinter.CTk()
+
+label = customtkinter.CtkLabel(root)
+
 #importing the methods I need
 import Function_Input
+
