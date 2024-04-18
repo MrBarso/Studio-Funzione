f55add615e62b03a90ccf594f58068eaa01d3a90
MrBarso <andbars2911@gmail.com>
2024-04-18 21:08:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-#in this file I combine every other file in order to do a complete study and graph of the function
 import customtkinter
 import tkinter
 import time
+import control as ct
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 
 #setting up graphic interface
@@ -17,6 +20,26 @@ root.grid_columnconfigure(1, weight=1)
 root.grid_columnconfigure((2, 3), weight=0)
 root.grid_rowconfigure((0, 1, 2), weight=1)
 
+function = "0"
+
+
+#layout of the final part
+def layout(function):
+    
+    # create sidebar frame (for the part after the function input)
+    sidebar_frame = customtkinter.CTkFrame(master = root, width=170, corner_radius=0)
+    sidebar_frame.grid(row=0, column=0, rowspan=20, sticky="nsew")
+    sidebar_frame.grid_rowconfigure(4, weight=1)
+    
+    #intro label
+    intro_label = customtkinter.CTkLabel(master = sidebar_frame, text = "FUNCTION EQUATION", text_color = "#1ED4B3")
+    intro_label.grid(pady=10, padx=10, column=0)
+
+    #label of the function
+    function_label = customtkinter.CTkLabel(master = sidebar_frame, text = "f(x) = " + function, text_color = "#1ED4B3")
+    function_label.grid(pady=2, padx=10, column=0)
+
+
 
 
 #input of the function
@@ -24,14 +47,16 @@ root.grid_rowconfigure((0, 1, 2), weight=1)
 #actual input method
 def input():
     time.sleep(1)
+
+    function = entry.get()
+    
+
     frame_input.destroy()
+    layout(function)
 
-    # create sidebar frame with widgets
-    root.sidebar_frame = customtkinter.CTkFrame(root, width=170, corner_radius=0)
-    root.sidebar_frame.grid(row=0, column=0, rowspan=10, sticky="nsew")
-    root.sidebar_frame.grid_rowconfigure(4, weight=1)
+    
 
-#graphic elements
+#graphic elements for the input
 frame_input = customtkinter.CTkFrame(master=root)
 frame_input.grid(pady = 15, padx = 24, row=0, column=1)
 
@@ -44,7 +69,7 @@ label.grid(column = 0, row = 1)
 entry = customtkinter.CTkEntry(master = frame_input)
 entry.grid(column = 1, row = 1, pady = 8, padx = 5)
 
-button = customtkinter.CTkButton(master = frame_input, text="Submit function", command=input)
+button = customtkinter.CTkButton(master = frame_input, text="Submit function", hover_color='#1b89e4', command=input)
 button.grid(pady = 8, padx = 5, row = 2, column = 1)
 
 
